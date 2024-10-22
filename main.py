b0c42372e10b572bf0cc7095c5b3dc606a255bc3
MP <pavlicic1marko@gmail.com>
2024-10-23 00:40:41 +0200

    add chat history to FE

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from ollama_ai_api import ask_ollama
 
 st.title("AI web chat bot")
 
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
 prompt = st.chat_input("Ask AI something")
 if prompt:
     answer = ask_ollama(prompt)
-    st.write(f"AI: {answer}")
+    st.session_state.messages.append({"user question":prompt,"AI answer":answer})
+    for question_answer in st.session_state.messages:
+        st.write(question_answer)
